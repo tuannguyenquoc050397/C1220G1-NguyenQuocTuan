aa568d819aedbaf55fdfc6a64bed141f0c24588c
tuannguyenquoc050397 <tuannguyenquoc15dt2@gmail.com>
2021-01-06 14:17:39 +0700

    add Bien_KDL_Toantu

---
+++ module 1/Bien_kieudulieu_toantu/thuchanh/ToanTu.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputWidth=prompt("nhap chieu dai");
+inputHeight=prompt("nhap chieu rong");
+let width=parseInt(inputWidth);
+let height=parseInt(inputHeight);
+let area=inputHeight*inputWidth;
+document.write("dien tich la: "+area);
